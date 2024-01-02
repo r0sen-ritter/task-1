030d28a8aceabd665cab14b66ad36ab2b4eaab0c
Safin Mahmud <safinmahmud@Safins-MacBook-Air.local>
2024-01-02 13:39:45 +0600

    update: tree layout expansion

--- Task1/src/App.jsx
+++ Task1/src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import './App.css'
-import Folder from './components/Folder'
+import React, { useState } from 'react';
+import { IoMdArrowDropright } from "react-icons/io";
+import { MdArrowDropDown } from "react-icons/md";
 
-function App() {
+const App = () => {
   const [folders, setFolders] = useState([]);
 
-  const addFolder = (parentIndex) => {
-    const name = window.prompt('Enter folder name');
+  const addFolder = (parentFolder = null) => {
+    const name = prompt('Enter folder name');
     if (name) {
-      const newFolders = [...folders];
-      const newFolder = <Folder name={name} />;
-      if (parentIndex != null) {
-        newFolders[parentIndex].props.children.push(newFolder);
+      const newFolder = { name, children: [], isOpen: false };
+      if (parentFolder) {
+        const newFolders = JSON.parse(JSON.stringify(folders)); // Deep copy
+        const parentInState = newFolders.find(folder => folder.name === parentFolder.name);
+        parentInState.children.push(newFolder);
+        setFolders(newFolders);
       } else {
-        newFolders.push(newFolder);
+        setFolders(prevFolders => [...prevFolders, newFolder]);
       }
-      setFolders(newFolders);
     }
+  };
+
+  const toggleFolder = (folder) => {
+    const newFolders = JSON.parse(JSON.stringify(folders)); // Deep copy
+    const folderInState = newFolders.find(f => f.name === folder.name);
+    folderInState.isOpen = !folderInState.isOpen;
+    setFolders(newFolders);
+  };
+
+  const Folder = ({ folder }) => {
+    const handleClick = () => {
+      toggleFolder(folder);
+    };
+
+    return (
+      <div>
+        <div onClick={handleClick}>
+          {folder.name} {folder.isOpen ? <MdArrowDropDown /> : <IoMdArrowDropright />}
+        </div>
+        {folder.isOpen && (
+          <>
+            {folder.children.map((child, index) => (
+              <div style={{ paddingLeft: "1.5rem" }} key={index}>
+                <Folder folder={child} />
+              </div>
+            ))}
+            <button onClick={() => addFolder(folder)}>Add Subfolder</button>
+          </>
+        )}
+      </div>
+    );
   };
 
   return (
     <div>
       {folders.map((folder, index) => (
-        <div key={index}>
-          {folder}
-          <button onClick={() => addFolder(index)}>Add Subfolder</button>
-        </div>
+        <Folder folder={folder} key={index} />
       ))}
-      <button onClick={() => addFolder(null)}>Add Folder</button>
+      <button onClick={() => addFolder()}>Add Folder</button>
     </div>
   );
-}
+};
 
 export default App;
